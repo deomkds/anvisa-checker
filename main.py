a78556ba1a8b4ea54a7eeead56e5dc5fd5d9bbf1
Marcus Vinícius <mv.cunha2009@gmail.com>
2024-12-08 21:08:28 -0300

    Preparação para processar múltiplas URLs.

--- main.py
+++ main.py
@@ -18,18 +18,14 @@ LOAD_TIME = 10
 
 log(f"Running on {config.OS.title()}.")
 
-TEST_URL = "https://consultas.anvisa.gov.br/#/documentos/tecnicos/253510002520205/"
-
 def create_webdriver():
     options = Options()
     options.add_argument("--headless")
-
     if config.OS == "win32":
         driver = webdriver.Edge(options=options)
     else:
         service = Service(WEBDRIVER_PATH)
         driver = webdriver.Firefox(service=service, options=options)
-
     return driver
 
 
@@ -44,21 +40,14 @@ def fetch_webpage(driver, page):
 
 
 def extract_content(html_content, target_info):
-    # target_info can be: "Expediente", "Data do Expediente".
-
     soup = BeautifulSoup(html_content, 'html.parser')
-
     infos_list = []
-
     entries = soup.find_all("label", string=target_info)
-
     for entry in entries:
         entry = entry.next_siblings
-
         for info in entry:
             if info != "\n":
                 infos_list.append(info.get_text())
-
     return infos_list
 
 
@@ -68,45 +57,46 @@ def add_to_csv(data, separator=";", filename="data.csv"):
         log_file.write(f"{data}{separator}")
 
 
+def extract_petitions(driver, url):
+    content = fetch_webpage(driver, url)
+    if content:
+        table = []
+        headers = [
+            "Expediente", "Data do Expediente", "Nº do Protocolo", "Situação atual", "Assunto",
+            "Dados da Publicação (RE - Data Resolução - DOU - Data Publicação)"
+        ]
+
+        for header in headers:
+            log(f"Extraíndo '{header}'")
+            extracted_content = extract_content(content, header)
+            if not extracted_content:
+                log(f"FATAL: Nenhuma informação encontrada.", bail=True)
+            table.append(extracted_content)
+
+        extracted_date = extract_content(content, "Data do Processo")
+        table[1].insert(0, extracted_date[0])
+
+        for num, header in enumerate(headers):
+            table[num].insert(0, header)
+
+        for col in range(len(table[0])):
+            log(f"Column: {col + 1}")
+            for row in range(len(headers)):
+                log(f"Row: {row + 1}")
+                info = table[row][col].replace("\n", " ").replace("?", "").strip()
+                log(f"Info: {info}")
+                sep = "\n" if row == 5 else ";"
+                add_to_csv(info, sep)
+
+    else:
+        log("FATAL: Falha na extração do conteúdo da página.")
+
+
 def main():
     driver = create_webdriver()
-    try:
-        content = fetch_webpage(driver, TEST_URL)
-
-        if content:
-            headers = [
-                "Expediente", "Data do Expediente", "Nº do Protocolo", "Situação atual", "Assunto",
-                "Dados da Publicação (RE - Data Resolução - DOU - Data Publicação)"
-            ]
-
-            table = []
-
-            for num in range(6):
-                extracted_content = extract_content(content, headers[num])
-                if not extracted_content:
-                    log(f"FATAL: Zero content retrieved from ANVISA's website.", bail=True)
-                table.append(extracted_content)
-
-            extracted_date = extract_content(content, "Data do Processo")
-            table[1].insert(0, extracted_date[0])
-
-            for num, header in enumerate(headers):
-                table[num].insert(0, header)
-
-            for col in range(len(table[0])):
-                log(f"Column: {col}")
-                for row in range(6):
-                    log(f"Row: {row}")
-                    info = table[row][col].replace("\n", " ").replace("?", "").strip()
-                    log(f"Info: {info}")
-                    sep = "\n" if row == 5 else ";"
-                    add_to_csv(info, sep)
-
-        else:
-            log("Falha na extração do conteúdo da página.")
-
-    finally:
-        driver.quit()
+    mepenox = "https://consultas.anvisa.gov.br/#/documentos/tecnicos/253510002520205/"
+    extract_petitions(driver, mepenox)
+    driver.quit()
 
 
 if __name__ == "__main__":
